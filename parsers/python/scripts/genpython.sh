d5af864e223c21d6fd3c5d664b69f6a092a90b78
Eric Prud'hommeaux <eric+github@w3.org>
2020-05-11 19:56:12 +0200

    ~ migrating parsers to separate repos

--- parsers/python/scripts/genpython.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Note: need antlr4.5.1 or later to run this!
-# see: http://www.antlr.org/download.html for details
-cp ../../../ShExDoc.g4 .
-antlr4 -Dlanguage=Python3 -package parser -o ../pyshexc/parser -no-listener -visitor ShExDoc.g4
-rm ShExDoc.g4
-
